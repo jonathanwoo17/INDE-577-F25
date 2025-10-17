a9440cc103b5b12eb31186281797db9e4736a2b1
Jonathan Woo <jonathanwoo@jonathans-air-2.dyndns.rice.edu>
2025-10-17 14:29:53 -0500

    Adding preprocess and postprocess files (pulling them out so that they are more reusable)

--- src/rice_ml/perceptron.py
+++ src/rice_ml/perceptron.py
@@ -15,15 +15,11 @@ Dependencies
 - numpy
 - pandas
 - matplotlib
-- seaborn
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
-
-sns.set_theme()
 
 class Perceptron:
     """
